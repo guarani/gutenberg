2a10f6caf192389d5c04138dddb9ed61e014f64d
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2020-02-17 16:29:06 +0100

    Block editor: Add documentation for BlockSettingsMenuControl

--- packages/block-editor/src/components/block-settings-menu/index.js
+++ packages/block-editor/src/components/block-settings-menu/index.js
@@ -24,7 +24,7 @@ import BlockModeToggle from './block-mode-toggle';
 import BlockHTMLConvertButton from './block-html-convert-button';
 import BlockUnknownConvertButton from './block-unknown-convert-button';
 import __experimentalBlockSettingsMenuFirstItem from './block-settings-menu-first-item';
-import BlockSettingsMenuGroup from '../block-settings-menu-group';
+import BlockSettingsMenuControls from '../block-settings-menu-controls';
 
 const POPOVER_PROPS = {
 	className: 'block-editor-block-settings-menu__popover',
@@ -135,7 +135,7 @@ export function BlockSettingsMenu( { clientIds } ) {
 										/>
 									) }
 								</MenuGroup>
-								<BlockSettingsMenuGroup.Slot
+								<BlockSettingsMenuControls.Slot
 									fillProps={ { onClose } }
 								/>
 								<MenuGroup>
