d81fa8f6d3cccaa8535d562521581d70c9dbe9b1
Paul Von Schrottky <paul.von.schrottky@automattic.com>
2020-04-16 16:44:39 -0400

    React Native web view showing unsupported block

--- packages/block-library/src/missing/edit.native.js
+++ packages/block-library/src/missing/edit.native.js
@@ -1,8 +1,16 @@
 /**
  * External dependencies
  */
-import { Platform, View, Text, TouchableWithoutFeedback } from 'react-native';
-import { requestUnsupportedBlockFallback } from 'react-native-gutenberg-bridge';
+import {
+	Platform,
+	View,
+	Text,
+	Button,
+	Modal,
+	SafeAreaView,
+	TouchableWithoutFeedback,
+} from 'react-native';
+import WebView from 'react-native-webview';
 
 /**
  * WordPress dependencies
@@ -23,7 +31,7 @@ import styles from './style.scss';
 export class UnsupportedBlockEdit extends Component {
 	constructor( props ) {
 		super( props );
-		this.state = { showHelp: false };
+		this.state = { showHelp: false, isModalVisible: false };
 		this.toggleSheet = this.toggleSheet.bind( this );
 		this.requestFallback = this.requestFallback.bind( this );
 	}
@@ -67,7 +75,7 @@ export class UnsupportedBlockEdit extends Component {
 
 	requestFallback() {
 		this.toggleSheet();
-		this.setState( { sendFallbackMessage: true } );
+		this.setState( { sendFallbackMessage: true, isModalVisible: true } );
 	}
 
 	renderSheet( title ) {
@@ -97,55 +105,168 @@ export class UnsupportedBlockEdit extends Component {
 				  __( "'%s' isn't yet supported on WordPress for iOS" );
 		const infoTitle = sprintf( titleFormat, title );
 
-		return (
-			<BottomSheet
-				isVisible={ this.state.showHelp }
-				hideHeader
-				onClose={ this.toggleSheet }
-				onModalHide={ () => {
-					if ( this.state.sendFallbackMessage ) {
-						// On iOS, onModalHide is called when the controller is still part of the hierarchy.
-						// A small delay will ensure that the controller has already been removed.
-						this.timeout = setTimeout( () => {
-							requestUnsupportedBlockFallback(
-								attributes.originalContent
-							);
-						}, 100 );
-						this.setState( { sendFallbackMessage: false } );
+		const runFirst = `
+
+		window.onSave = () => {
+			const blocks = window.wp.data.select('core/block-editor').getBlocks();
+			const HTML = window.wp.blocks.serialize( blocks );
+			window.ReactNativeWebView.postMessage(HTML);
+		}
+
+		window.insertBlock = () => {
+			window.setTimeout(() => {
+                const blockHTML = '${ attributes.originalContent.replace(
+					/\n/g,
+					'\\n'
+				) }';
+                let blocks = window.wp.blocks.parse(blockHTML);
+                window.wp.data.dispatch('core/block-editor').resetBlocks(blocks);
+			}, 0);
+		};
+		
+			window.onload = () => {
+			const wpAdminBar = document.getElementById('wpadminbar');
+			const wpToolbar = document.getElementById('wp-toolbar');
+			if (wpAdminBar) {
+				wpAdminBar.style.display = 'none';
+			}
+			if (wpToolbar) {
+				wpToolbar.style.display = 'none';
+			}
+		
+			const content = document.getElementById('wpbody-content');
+			if (content) {
+				const callback = function(mutationsList, observer) {
+					const header = document.getElementsByClassName("edit-post-header")[0];
+					const postTitle = document.getElementById('post-title-0');
+					if (postTitle && header.id == '') {
+						header.id = 'gb-header';
+						header.style.height = 0;
+						postTitle.style.display = 'none';
+						Array.from(header.children).forEach( (child) => {
+							child.style.display = 'none';
+						});
+		
+						const headerToolbar = header.getElementsByClassName('edit-post-header-toolbar')[0];
+						headerToolbar.style.display = null;
+						headerToolbar.parentNode.style.display = null;
+						const inserterToggle = header.getElementsByClassName('block-editor-inserter__toggle')[0];
+						inserterToggle.style.display = 'none';
+		
+						const blockToolbar = header.getElementsByClassName('edit-post-header-toolbar__block-toolbar')[0];
+						blockToolbar.style.top = '0px';
+		
+						const skeleton = document.getElementsByClassName('block-editor-editor-skeleton')[0];
+		//                skeleton.style.top = '0px';
+		
+						const appender = document.getElementsByClassName('block-list-appender')[0];
+						if (appender && appender.id == '') {
+							appender.id = 'appender';
+							appender.style.display = 'none';
+						}
+		
+						window.insertBlock();
+						observer.disconnect();
 					}
-				} }
-			>
-				<View style={ styles.infoContainer }>
-					<Icon
-						icon={ help }
-						color={ infoSheetIconStyle.color }
-						size={ styles.infoSheetIcon.size }
-					/>
-					<Text style={ [ infoTextStyle, infoTitleStyle ] }>
-						{ infoTitle }
-					</Text>
-					<Text style={ [ infoTextStyle, infoDescriptionStyle ] }>
-						{ __(
-							'We are working hard to add more blocks with each release. In the meantime, you can also edit this post on the web.'
-						) }
-					</Text>
-				</View>
-				{ // eslint-disable-next-line no-undef
-				__DEV__ && (
-					<>
-						<BottomSheet.Cell
-							label={ __( 'Edit post on Web Browser' ) }
-							separatorType="topFullWidth"
-							onPress={ this.requestFallback }
+				};
+				const observer = new MutationObserver(callback);
+				const config = { attributes: true, childList: true, subtree: true };
+				observer.observe(content, config);
+			}
+		  }
+		  `;
+
+		return (
+			<>
+				<Modal
+					animationType="slide"
+					presentationStyle="fullScreen"
+					visible={ this.state.isModalVisible }
+					onRequestClose={ () =>
+						this.setState( { isModalVisible: false } )
+					}
+				>
+					<SafeAreaView
+						style={ { flex: 1, backgroundColor: 'black' } }
+					>
+						{ /* <View> */ }
+						<Button
+							onPress={ () =>
+								this.setState( { isModalVisible: false } )
+							}
+							title="Close"
+							color="white"
 						/>
-						<BottomSheet.Cell
-							label={ __( 'Dismiss' ) }
-							separatorType="topFullWidth"
-							onPress={ this.toggleSheet }
+						<Button
+							onPress={ () =>
+								this.webref.injectJavaScript(
+									'window.onSave()'
+								)
+							}
+							title="Save"
+							color="white"
 						/>
-					</>
-				) }
-			</BottomSheet>
+						{ /* </View> */ }
+						<WebView
+							ref={ ( r ) => ( this.webref = r ) }
+							source={ {
+								uri:
+									'https://cruisinglamb.wordpress.com/wp-admin/post-new.php',
+							} }
+							injectedJavaScript={ runFirst }
+							// injectedJavaScriptForMainFrameOnly={false}
+							onMessage={ ( event ) => {
+								attributes.originalContent =
+									event.nativeEvent.data;
+								this.setState( { isModalVisible: false } );
+							} }
+						/>
+					</SafeAreaView>
+				</Modal>
+				<BottomSheet
+					isVisible={ this.state.showHelp }
+					hideHeader
+					onClose={ this.toggleSheet }
+				>
+					<View style={ styles.infoContainer }>
+						<Icon
+							icon={ help }
+							color={ infoSheetIconStyle.color }
+							size={ styles.infoSheetIcon.size }
+						/>
+						<Text style={ [ infoTextStyle, infoTitleStyle ] }>
+							{ infoTitle }
+						</Text>
+						<Text style={ [ infoTextStyle, infoDescriptionStyle ] }>
+							{ __(
+								'We are working hard to add more blocks with each release. In the meantime, you can also edit this post on the web.'
+							) }
+						</Text>
+					</View>
+					{ // eslint-disable-next-line no-undef
+					__DEV__ && (
+						<>
+							<BottomSheet.Cell
+								label={ __( 'Edit post on Web Browser' ) }
+								separatorType="topFullWidth"
+								onPress={ () => {
+									this.toggleSheet();
+									this.timeout = setTimeout( () => {
+										this.setState( {
+											isModalVisible: true,
+										} );
+									}, 1000 );
+								} }
+							/>
+							<BottomSheet.Cell
+								label={ __( 'Dismiss' ) }
+								separatorType="topFullWidth"
+								onPress={ this.toggleSheet }
+							/>
+						</>
+					) }
+				</BottomSheet>
+			</>
 		);
 	}
 
